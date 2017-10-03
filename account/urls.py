5cb275cfd780921a733ec428778cb55ba5637a94
nk910216 <nk910216@gmail.com>
2017-10-03 23:09:08 +0800

    change password templates and url

--- account/urls.py
+++ account/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     url(r'^signup/$', signup, name='account_signup'), # for sign up
     url(r'^login/$', auth_view.LoginView.as_view(template_name='login.html'),
         name='account_login'),
-    url(r'^logout/$', auth_view.LogoutView.as_view(), name='account_logout')
+    url(r'^logout/$', auth_view.LogoutView.as_view(), name='account_logout'),
+    url(r'^password/$', 
+        auth_view.PasswordChangeView.as_view(template_name='password_change.html'),
+        name='account_password_change'),
+    url(r'^password/done/$', auth_view.PasswordChangeDoneView.as_view(
+            template_name='password_change_done.html'), name='password_change_done')
 ]
