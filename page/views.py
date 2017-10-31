feeff06fe644cd169bc5ce929ac5433d2add5e6b
nk910216 <nk910216@gmail.com>
2017-11-01 00:52:19 +0800

    add latest book in homepage

--- page/views.py
+++ page/views.py
@@ -1,8 +1,25 @@
+from collections import defaultdict
+
 from django.shortcuts import render
 
+from book.models import BookItem
 from .forms import SearchForm
 
 # Create your views here.
 def pagehome(request):
+    ITEM_COUNT_PER_USER = 3
+    # search form
     search_form = SearchForm()
-    return render(request, 'home.html', {'search_form': search_form})
+    # latest book item
+    book_items = BookItem.objects.filter(is_valid=True).order_by('-created_at')
+    item_list = []
+    user_items_count = defaultdict(int)
+    for item in book_items:
+        username = item.owner.username
+        count = user_items_count[username]
+        print(item)
+        if count < ITEM_COUNT_PER_USER:
+            item_list.append(item)
+            user_items_count[username] += 1
+
+    return render(request, 'home.html', {'search_form': search_form, 'lastest_book_items': item_list})
